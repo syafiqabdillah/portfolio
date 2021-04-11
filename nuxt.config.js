7ca85e97041d96d55b85c84f0ca617687dd14677
syafiq <abdillah.syafiq@gmail.com>
2021-04-11 13:47:03 +0800

    pake netlify aja udah

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,34 +5,27 @@ export default {
   head: {
     title: 'Syafiq Abdillah U',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/global.css',
-    '~/assets/icofont/icofont.min.css',
-  ],
+  css: ['~/assets/css/global.css', '~/assets/icofont/icofont.min.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -46,12 +39,11 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   target: 'static',
 
-  router: {
-    base: 'portofolio'
-  }
+  generate: {
+    fallback: true,
+  },
 }
