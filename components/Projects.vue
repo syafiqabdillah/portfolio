ba941f8cd5ae8c6d84c23ae16543309502bf67fb
syafiqabdillah <abdillah.syafiq@gmail.com>
2022-08-08 10:31:47 +0700

    update projects

--- components/Projects.vue
+++ components/Projects.vue
@@ -21,44 +21,61 @@ export default {
       projects: [
         {
           name: 'Makanapaya',
-          desc:
-            'Kasih saran menu makanan minuman buat menjawab pertanyaan "makan apa yak?" dari teman kau',
+          desc: 'A website that helps you think of what to eat or drink',
           src: require('~/assets/img/makanapaya.png'),
           url: 'https://www.makanapaya.my.id',
-          tech: ['Next.js', 'React.js', 'MongoDB', 'Express.js'],
+          tech: [
+            'Next.js',
+            'React.js',
+            'Progressive Web Apps',
+            'MongoDB',
+            'Express.js',
+            'Mixpanel',
+            'Sentry',
+          ],
         },
         {
           name: 'Kenal Kampus',
-          desc: 'Kumpulan cerita pengalaman kuliah',
+          desc:
+            'Collection of stories from students in universities all over Indonesia. It helps high school student to choose where to pursue their study.',
           src: require('~/assets/img/kenalkampus.png'),
           url: 'https://www.kenalkampus.com',
-          tech: ['Nuxt.js', 'Vue.js', 'MongoDB', 'Express.js'],
+          tech: [
+            'Nuxt.js',
+            'Vue.js',
+            'Progressive Web Apps',
+            'MongoDB',
+            'Express.js',
+            'Mixpanel',
+          ],
         },
         {
           name: 'Jlantah Hero',
-          desc: 'Tuker minyak jelantah jadi uang',
+          desc:
+            'A mobile-first web app that lets you convert your used vegetable oil into money or new oils. It was our submission in Indonesia Digital Tribe 2021-2022 event that led us to Top 7 position out of 2000+ teams.',
           src: require('~/assets/img/jlantahhero.png'),
           url: 'https://www.jlantahhero.com',
-          tech: ['Next.js', 'React.js', 'MongoDB', 'Express.js'],
+          tech: ['Next.js', 'React.js', 'MongoDB', 'Express.js', 'Mixpanel'],
         },
         {
           name: 'Foodle',
-          desc: 'Kayak wordle tapi kata-katanya dari Makanapaya',
+          desc:
+            'My spin-off of the viral word game in 2021-2022, Wordle, but the words are from Makanapaya.',
           src: require('~/assets/img/foodle.png'),
           url: 'https://www.makanapaya.my.id/foodle',
           tech: ['Next.js', 'React.js', 'MongoDB', 'Express.js'],
         },
         {
           name: 'Makanapaya Android',
-          desc: 'Android version of Makanapaya',
+          desc: 'Android version of Makanapaya.',
           src: require('~/assets/img/makanapayaandroid.jpg'),
           url:
             'https://play.google.com/store/apps/details?id=com.syafiqau.makanapaya',
-          tech: ['Flutter', 'Dart', 'MongoDB', 'Express.js'],
+          tech: ['Flutter', 'Dart', 'MongoDB', 'Express.js', 'Mixpanel'],
         },
         {
           name: 'Expensez',
-          desc: 'Pencatat pengeluaran harian tanpa gimmick',
+          desc: 'A simple expense tracker application.',
           src: require('~/assets/img/expensez.jpg'),
           url:
             'https://play.google.com/store/apps/details?id=com.syafiqabdillah.expensez',
@@ -66,9 +83,9 @@ export default {
         },
         {
           name: 'Wedding Invitation',
-          desc: 'Minimalist and ellegant wedding invitation',
+          desc: 'Minimalist and ellegant wedding invitation.',
           src: require('~/assets/img/wedding.png'),
-          url: 'https://afifasyafiq.netlify.app',
+          url: 'https://afifasyafiqjourney.my.id',
           tech: ['Next.js', 'React.js', 'MongoDB', 'Express.js'],
         },
       ],
